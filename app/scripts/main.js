fc59be8ab60ea73cc7d2fd7933977643a809aa88
Alex Ridore <aridore@grey.com>
2016-07-25 17:19:47 -0400

    Initial commit of all files

---
+++ app/scripts/main.js
@@ -0,0 +1,50 @@
+// pure JS
+var elem = document.getElementById('mySwipe');
+window.mySwipe = Swipe(elem, {
+  continuous: false,
+  disableScroll: true,
+  callback: function(index, element) {
+  	$('.movie-nav li').removeClass('active');
+
+  	var slidePos = mySwipe.getPos() + 1;
+
+  	$('.movie-nav li:nth-child('+ slidePos +')').addClass('active');
+  }
+});
+
+$('.movie-nav li').click(function() {
+	$('.movie-nav li').removeClass('active');
+	
+	var index = $('.movie-nav li').index(this);
+
+	mySwipe.slide(index, 400);
+	$(this).addClass('active');
+});
+
+var apiURL = 'data.json';
+
+new Vue({
+
+  el: '#app',
+
+  data: {
+    currentBranch: 'dev',
+    items: null
+  },
+
+  created: function () {
+    this.fetchData();
+  },
+
+  methods: {
+    fetchData: function () {
+    var self = this;
+    $.get( apiURL, function( data ) {
+        self.items = data;
+        console.log(data);
+    });
+
+    }
+
+  }
+});
